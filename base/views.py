ddc1bc575fb8446bfb2f835da91726954f1316cc
Arjun Pandey <arjun_bp@rediffmail.com>
2022-03-12 14:09:34 +0530

    second commit

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse
-from .models import Room ,Topic
+from .models import Room ,Topic , Messages
 from django.contrib.auth.models import User
 from .forms import RoomForm
 from django.db.models import Q 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages 
 from django.contrib.auth import  authenticate ,login , logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -15,8 +16,9 @@ def loginPage(request):
     # if request.user.is_authenticated():
     #     return redirect('home')
 
+    page = 'login'
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -35,13 +37,29 @@ def loginPage(request):
 
 
 
-    context = {}
+    context = {'page':page}
     return render(request,'base/login_register.html',context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'An error occurred while registration')
+
+    return render(request,'base/login_register.html',{'form':form})
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q') != None else ''
@@ -61,7 +79,17 @@ def home(request):
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context = {'room':room}
+    roomMessage=room.messages_set.all().order_by('-created') #give us the set of messages related to this room
+    
+    if request.method == 'POST':
+        message=Messages.objects.create( 
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room',pk=room.id)
+
+    context = {'room':room, 'roomMessages':roomMessage}
     return render(request, 'base/room.html',context)
 
 @login_required(login_url='login')
@@ -75,7 +103,7 @@ def createRoom(request):
 
     context={'form': form}
     return render(request, 'base/room_form.html' , context)
-
+   
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
